fe1efd58c7b62366c3aafbe47a77f1a462753d6c
Luka T <asd@asd.com>
2022-12-21 11:16:03 +0100

    Dodana nula v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 const App = () => {
-    const broj = 3;
+    const broj = 5;
+
     
 
     return (
         <div>
             <div>
-                {broj % 2 == 0 && <p>{broj} je paran.</p>}
-                {broj % 2 != 0 && <p>{broj} je neparan.</p>}
+                {(broj % 2 == 0 && broj !== 0) && <p>{broj} je paran.</p>}
+                {(broj % 2 != 0 && broj !== 0) && <p>{broj} je neparan.</p>}
                 {broj === 0 && <p>Zadani broj je nula.</p>}
             </div>
         </div>
