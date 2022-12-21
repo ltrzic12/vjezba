117ed0c40ae52a6deea90e16600e3e612575e6c2
Luka T <asd@asd.com>
2022-12-21 11:10:16 +0100

    Dodan slucaj za nulu

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
     return (
         <div>
             <div>
-                {broj % 2 == 0 && <p>{broj} je paran</p>}
-                {broj % 2 != 0 && <p>{broj} je neparan</p>}
+                {broj % 2 == 0 && <p>{broj} je paran.</p>}
+                {broj % 2 != 0 && <p>{broj} je neparan.</p>}
+                {broj === 0 && <p>Zadani broj je nula.</p>}
             </div>
         </div>
     );
